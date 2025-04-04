bf5e4c1bcea80f86d9fd2cb4016ce708151ff7c0
ujjval <ujjaval.i@agileinfoways.com>
2025-04-04 11:29:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/smart_cricket_tournament/SmartCricketTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_cricket_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCricketTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCricketTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_cricket_tournament/SmartCricketTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_cricket_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCricketTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
